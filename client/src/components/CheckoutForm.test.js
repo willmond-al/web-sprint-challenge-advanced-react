c6bf50527e9cff3303eb66dbeea60ff1dfd126c8
willmondal <wbmondal@aol.com>
2021-01-15 14:30:20 -0400

    test code added, cannot be checked

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,11 +1,34 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, getByRole } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from '@testing-library/user-event'
 
 // Write up the two tests here and make sure they are testing what the title shows
 
 test("form header renders", () => {
     render(<CheckoutForm/>)
+    let header = screen.getByRole("h2")
+    expect(header).toBeInTheDocument();
 });
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    const firstNameInput = screen.getByLabelText(/firstname/i);
+    const lastNameInput = screen.getByLabelText(/lastname/i);
+    const addressInput = screen.getByLabelText(/address/i);
+    const cityInput = screen.getByLabelText(/city/i);
+    const stateInput = screen.getByLabelText(/state/i);
+    const zipInput = screen.getByLabelText(/zip/i);
+
+    userEvent.type(firstNameInput, "test")
+    userEvent.type(lastNameInput, "test")
+    userEvent.type(addressInput, "test")
+    userEvent.type(cityInput, "test")
+    userEvent.type(stateInput, "test")
+    userEvent.type(zipInput, "test")
+
+    const button = screen.getByRole("button")
+    userEvent.click(button)
+
+    const successMessage = screen.findAllByText("test")
+    expect(successMessage).toBeInTheDocument()
+});
