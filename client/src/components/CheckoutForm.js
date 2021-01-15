0279f45d364fa130888539c1f366e947f48954f6
willmondal <wbmondal@aol.com>
2021-01-15 13:47:42 -0400

    useForm filled to handle changes and added into checkoutForm

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -15,12 +15,9 @@ const initialValue = {
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  const [showSuccessMessage, setShowSuccessMessage] = useForm(false);
+  const [values, setValues, handleChanges] = useForm(initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,17 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react'
+import localStorage from './useLocalStorage'
+import e from 'cors';
+
+const useForm = (intialValue) =>{
+    const [value, setValue] = useState(intialValue);
+    const handleChanges = evt =>{
+        setValue({
+            ...value, 
+            [evt.target.name]: evt.target.value
+        })
+    }
+    return ([value, setValue, handleChanges])
+}
+
+export default useForm
